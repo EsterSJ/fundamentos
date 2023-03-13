92f4a127be963e20fd34125ade9ebdbbdcaf24a7
EsterSJ <ester_s@hotmail.com>
2023-03-13 18:34:54 +0100

    Añadida funcion cuadrado de un numero

---
+++ index.js
@@ -0,0 +1,7 @@
+suma = (op1, op2) => {return op1 + op2};
+resta = (op1, op2) => {return op1 - op2};
+multiplicacion = (op1, op2) => {return op1 * op2};
+division = (op1, op2) => {return op1 / op2};
+cuadrado = (op1) => {return op1*op1};
+
+module.exports = {suma, resta, multiplicacion, division, cuadrado};
---
+++ run.js
@@ -0,0 +1,9 @@
+let myLib = require("./index");
+
+let num1 = 20;
+let num2 = 8;
+
+console.log(myLib.suma(num1,num2)); 
+console.log(myLib.resta(num1,num2));
+console.log(myLib.multiplicacion(num1,num2));
+console.log(myLib.division(num1,num2));
