89fe186056de1da2ad74a6d3ec05c29bd7411148
EsterSJ <ester_s@hotmail.com>
2023-03-14 09:59:34 +0100

    eliminar suma

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-suma = (op1, op2) => {return op1 + op2};
 resta = (op1, op2) => {return op1 - op2};
 multiplicacion = (op1, op2) => {return op1 * op2};
 division = (op1, op2) => {return op1 / op2};
 cuadrado = (op1) => {return op1*op1};
 
-module.exports = {suma, resta, multiplicacion, division, cuadrado};
+module.exports = {resta, multiplicacion, division, cuadrado};
